684c11e2c7e728e64a5917c4c7a288e93ae68002
MohammadBnei <mohammadamine.banaei@pm.me>
2025-04-15 18:36:26 +0200

    working on stuff

--- src/lib/component/ui/badge.svelte
+++ src/lib/component/ui/badge.svelte
@@ -15,3 +15,9 @@
 	class="inline-flex items-center rounded-md {color} px-2 py-1 text-xs ring-1 ring-inset hover:skew-y-2 transition"
 	>{content.toLocaleUpperCase()}</span
 >
+
+<style lang="postcss">
+	.none {
+		@apply ring-base-200 dark:ring-gray-500;
+	}
+</style>
